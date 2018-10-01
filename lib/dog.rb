22598c55b2d1cd99b08c330b1d773e6ef39b810b
Gina Fedder <ginafedder@yahoo.com>
2018-10-01 21:20:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.all 
+    @@all.each {|name| puts name.name}
+  end 
+end 
+
